185aa783917ab8aea71b998135a0aa79905c20bb
vo0a <vo0a195@gmail.com>
2021-09-17 21:03:40 +0900

    first commit

---
+++ coin/src/main/java/com/coincommunity/api/coin/CoinApplication.java
@@ -0,0 +1,13 @@
+package com.coincommunity.api.coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinApplication.class, args);
+	}
+
+}

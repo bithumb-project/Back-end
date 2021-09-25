633398137f95e5073f3176aa26f4b79cfdc08ba5
MinChul-Son <smc5236@naver.com>
2021-09-25 20:47:08 +0900

    feature : 닉네임 중복 확인 추가

--- security/src/main/java/com/coincommunity/api/security/repository/MemberRepository.java
+++ security/src/main/java/com/coincommunity/api/security/repository/MemberRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(final String email);
+    boolean existsByNickname(final String nickname);
 }

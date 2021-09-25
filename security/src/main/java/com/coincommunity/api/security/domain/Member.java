dc87146e9464c439758a79cf50038b49d73b14e7
MinChul-Son <smc5236@naver.com>
2021-09-25 19:28:43 +0900

    feature : Member 엔티티

---
+++ security/src/main/java/com/coincommunity/api/security/domain/Member.java
@@ -0,0 +1,39 @@
+package com.coincommunity.api.security.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column
+    private String profile;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    List<Role> roles = new ArrayList<>();
+}

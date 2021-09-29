c8ba75d0cc78141b682abe1449a60c761fa8a534
MinChul-Son <smc5236@naver.com>
2021-09-29 20:30:39 +0900

    feature : 시큐리티 설정

--- security/src/main/java/com/coincommunity/api/security/config/WebSecurityConfig.java
+++ security/src/main/java/com/coincommunity/api/security/config/WebSecurityConfig.java
@@ -1,10 +1,15 @@
 package com.coincommunity.api.security.config;
 
+import com.coincommunity.api.security.handler.SecurityAccessDeniedHandler;
+import com.coincommunity.api.security.provider.SecurityProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,16 +17,41 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final SecurityProvider provider;
+    private final SecurityAuthenticationEntryPoint entryPoint;
+    private final SecurityAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "*/**")
+            .antMatchers("/", "/h2-console/**");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable()
+            .exceptionHandling()
+            .authenticationEntryPoint(entryPoint)
+            .accessDeniedHandler(accessDeniedHandler)
 
-        http.authorizeRequests()
-            .antMatchers("*").permitAll();
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/v1/member").permitAll()
+            .antMatchers("/api/v1/login").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(new SecurityConfig(provider));
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
 }

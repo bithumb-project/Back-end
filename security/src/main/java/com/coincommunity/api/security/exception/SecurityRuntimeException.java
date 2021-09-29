fcb6b9e21ea6b79a73d66339eef88be27d73f84f
MinChul-Son <smc5236@naver.com>
2021-09-29 20:31:32 +0900

    feature : 시큐리티 통합 Exception
    - 추후에 세부적으로 예외처리로 변경 예정

---
+++ security/src/main/java/com/coincommunity/api/security/exception/SecurityRuntimeException.java
@@ -0,0 +1,15 @@
+package com.coincommunity.api.security.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class SecurityRuntimeException extends RuntimeException{
+
+    private static final long SerializableUID = 1L;
+
+    private final String msg;
+    private final HttpStatus httpStatus;
+}
